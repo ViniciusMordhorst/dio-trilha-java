92e99215049187a7df471728313157976eb25819
ViniciusMordhorst <viniciuskahlermordhorst@hotmail.com>
2024-05-03 00:06:30 -0300

    Operadores Ternarios

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,39 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        
-        String nomeCompleto = "LINGUEGEM " + "JAVA";
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
         System.out.println(nomeCompleto);
-
+    /*
+        Operadores
         int numero = 5;
         System.out.println(- numero);
+
+        int num = 5;
+
+        System.out.println(++ num);
+   */
+
+   /* Imprime false
+      boolean variavel = true;
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+     */
+
+
+    /* 
+        EXPRESSÃO BOLEANA TRUE OR FALSE
+    OPERADOR TERNÁRIO IF ELSE REDUZIDO
+    int a, b;
+        a=5;
+        b=6;
+
+    String resultado = a==b ?"Verdadeiro" : "Falso";
+        System.out.println(resultado);
+    */
+
+        
 
     }
 

ddc9ac227c921c8e851f0b70743f2f69656b1a10
ViniciusMordhorst <viniciuskahlermordhorst@hotmail.com>
2024-05-22 10:43:31 -0300

    Desafio POO

---
+++ poo-desafio/src/iPhone.java
@@ -0,0 +1,71 @@
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // Métodos de ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Métodos de AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Métodos de NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    // Método main para testar as funcionalidades
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando ReprodutorMusical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+
+        // Testando AparelhoTelefonico
+        meuIphone.ligar("123-456-7890");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando NavegadorInternet
+        meuIphone.exibirPagina("http://www.example.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
